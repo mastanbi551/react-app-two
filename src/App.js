e49732ea80a185318ef1821773f855ecb8503bee
chaitanya <Chaitanya.Katepalli@vitalitygroup.com>
2023-07-13 15:20:52 +0530

    basic change through github deploy

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello 1 New React App</p>
+        <p>Hello 2 New React App</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
